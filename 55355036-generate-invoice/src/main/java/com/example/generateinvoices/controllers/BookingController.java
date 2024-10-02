17874c0b5dac8876e8688acf7190592f18850b11
mryash08 <140238011+mryash08@users.noreply.github.com>
2024-10-02 15:41:16 +0530

    Add files via upload

--- 55355036-generate-invoice/src/main/java/com/example/generateinvoices/controllers/BookingController.java
+++ 55355036-generate-invoice/src/main/java/com/example/generateinvoices/controllers/BookingController.java
@@ -2,6 +2,8 @@ package com.example.generateinvoices.controllers;
 
 import com.example.generateinvoices.dtos.GenerateInvoiceRequestDto;
 import com.example.generateinvoices.dtos.GenerateInvoiceResponseDto;
+import com.example.generateinvoices.dtos.ResponseStatus;
+import com.example.generateinvoices.models.Invoice;
 import com.example.generateinvoices.services.BookingService;
 
 public class BookingController {
@@ -12,6 +14,15 @@ public class BookingController {
     }
 
     public GenerateInvoiceResponseDto generateInvoice(GenerateInvoiceRequestDto requestDto) {
-        return null;
+        GenerateInvoiceResponseDto responseDto = new GenerateInvoiceResponseDto();
+        try {
+            Invoice invoice = bookingService.generateInvoice(requestDto.getUserId());
+            responseDto.setInvoice(invoice);
+            responseDto.setResponseStatus(ResponseStatus.SUCCESS);
+            return responseDto;
+        } catch (Exception e) {
+            responseDto.setResponseStatus(ResponseStatus.FAILURE);
+            return responseDto;
+        }
     }
 }
